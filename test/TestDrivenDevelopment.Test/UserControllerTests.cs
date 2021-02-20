abd721a18ac744f2d69c16192c17fcb964d7bb19
mostmand <mostmand.mh@gmail.com>
2021-02-20 23:44:20 +0330

    Add AddUser happy path test

---
+++ test/TestDrivenDevelopment.Test/UserControllerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NSubstitute;
+using TestDrivenDevelopment.Models;
+using TestDrivenDevelopment.Repository;
+using Xunit;
+
+namespace TestDrivenDevelopment.Test
+{
+    public class UserControllerTests
+    {
+        private readonly UserController _sut;
+        private readonly IUserRepository _userRepository;
+
+        public UserControllerTests()
+        {
+            _userRepository = Substitute.For<IUserRepository>();
+            _sut = new UserController(_userRepository);
+        }
+
+        [Fact]
+        public void AddUser_ShouldAddUser_WhenAllInputsAreValid()
+        {
+            // Arrange
+            var user = new User()
+            {
+                Email = "asghar@gmail.com",
+                FirstName = "Asghar",
+                LastName = "Asghari",
+                DateOfBirth = new DateTime(1980, 1, 1)
+            };
+
+            // Act
+            _sut.AddUser(user);
+
+            //Assert
+            _userRepository.Received(1).AddUser(user);
+        }
+    }
+}
