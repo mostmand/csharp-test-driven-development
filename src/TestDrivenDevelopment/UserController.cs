0ec07f4b47a4409012dbd4b2d823912b147dee20
mostmand <mostmand.mh@gmail.com>
2021-02-20 23:26:53 +0330

    Add UserController.cs

---
+++ src/TestDrivenDevelopment/UserController.cs
@@ -0,0 +1,17 @@
+﻿using TestDrivenDevelopment.Models;
+
+namespace TestDrivenDevelopment
+{
+    public class UserController : IUserController
+    {
+        public User AddUser(User user)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public User GetUserById(string userId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
